5400bdf9c978a319c3239b0cc8a4d1838d0d9a27
yamami mohamed monsif <mohammedyamami05@gmail.com>
2025-04-08 22:57:53 +0100

    added animations to testimonials section

--- components/Testimonials.tsx
+++ components/Testimonials.tsx
@@ -1,16 +1,36 @@
 import Section from "./Section";
 import Image from "next/image";
+import { motion } from "motion/react";
 
 const Testimonials = () => {
   return (
     <Section>
       <div className="flex flex-col items-center gap-11">
-        <h2>Testimonials</h2>
-        <p className="paragraph-large text-center">
+        <motion.h2
+          initial={{ y: 50, opacity: 0 }}
+          whileInView={{ y: 0, opacity: 1 }}
+          transition={{ duration: 0.6, ease: "easeOut" }}
+          viewport={{ once: true, amount: 0.5 }}
+        >
+          Testimonials
+        </motion.h2>
+        <motion.p
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeOut", delay: 0.2 }}
+          viewport={{ once: true, amount: 0.5 }}
+          className="paragraph-large text-center"
+        >
           Most calendars are designed for teams. Slate is designed for
           freelancers who want a simple way to plan their schedule.
-        </p>
-        <div className="flex items-center gap-2.5">
+        </motion.p>
+        <motion.div
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.6, ease: "easeOut", delay: 0.4 }}
+          viewport={{ once: true, amount: 0.5 }}
+          className="flex items-center gap-2.5"
+        >
           <Image
             src="/assets/profile-image.webp"
             alt="Testimonial 1"
@@ -22,7 +42,7 @@ const Testimonials = () => {
             <p>Organize across</p>
             <p>UI Designer</p>
           </div>
-        </div>
+        </motion.div>
       </div>
     </Section>
   );
